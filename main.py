93f8b8b2b1e549d72f83857702824ef2eae2b099
harry <harry.rw96@gmail.com>
2022-09-23 16:07:07 +1000

    print_board function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Global Variables
 
 board = ["-", "-", "-",
-        "-", "", "-",
+        "-", "-", "-",
         "-", "-", "-"]
 
 current_player = "X"
@@ -9,6 +9,14 @@ winner = None
 game_running = True
 
 # Display the gameboard
+def print_board(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+print_board(board)
+    
+
 
 # Receive player input
 
